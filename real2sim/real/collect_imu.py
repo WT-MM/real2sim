766c6ee4461432a65ef087db27735b66b64278fb
WT-MM <wesley.maa@gmail.com>
2024-12-06 05:06:07 +0000

    script

--- real2sim/real/collect_imu.py
+++ real2sim/real/collect_imu.py
@@ -3,25 +3,33 @@ import time
 import numpy as np
 from imu import HexmoveImuReader
 
-imu_reader = HexmoveImuReader("can0", 1, 1)
+def collect_imu_data(imu_config: dict[str, any], path: str):
+    assert "port" in imu_config
+    assert "canid" in imu_config
+    assert "id" in imu_config
 
-num_samples = 50  # 10 Hz for 5 seconds
-angles = []
-print("Calculating IMU offset...")
-for _ in range(num_samples):
-    imu_data = imu_reader.get_data()
-    angles.append([imu_data.x_angle, imu_data.y_angle, imu_data.z_angle])
-    time.sleep(0.1)
+    imu_reader = HexmoveImuReader(imu_config["port"], imu_config["canid"], imu_config["id"])
 
-angular_offset = np.mean(angles, axis=0)
-
-log_data = []
-try:
-    while True:
+    num_samples = 50  # 10 Hz for 5 seconds
+    angles = []
+    print("Calculating IMU offset...")
+    for _ in range(num_samples):
         imu_data = imu_reader.get_data()
-        log_data.append([imu_data.x_angle - angular_offset[0], imu_data.y_angle - angular_offset[1], imu_data.z_angle - angular_offset[2]])
-        time.sleep(0.01)
-except KeyboardInterrupt:
-    pass
+        angles.append([imu_data.x_angle, imu_data.y_angle, imu_data.z_angle])
+        time.sleep(0.1)
 
-np.save("imu_log.npy", np.array(log_data))
+    angular_offset = np.mean(angles, axis=0)
+
+    print(f"IMU offset: {angular_offset}")
+
+    log_data = []
+    print("Collecting IMU data... Press Ctrl+C to stop")
+    try:
+        while True:
+            imu_data = imu_reader.get_data()
+            log_data.append([imu_data.x_angle - angular_offset[0], imu_data.y_angle - angular_offset[1], imu_data.z_angle - angular_offset[2]])
+            time.sleep(0.01)
+    except KeyboardInterrupt:
+        pass
+
+    np.save(path, np.array(log_data))
---
+++ real2sim/scripts/read_imu.py
@@ -0,0 +1,8 @@
+from real2sim.real.collect_imu import collect_imu_data
+
+
+if __name__ == "__main__":
+    collect_imu_data(
+        {"port": "can0", "canid": 1, "id": 1},
+        "imu_log.npy",
+    )
