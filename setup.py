3f61360c970efe0d227e77eac9ec81f923a6e8fe
Wesley Maa <wesley.maa@gmail.com>
2024-12-14 22:59:04 -0800

    update reqs

--- setup.py
+++ setup.py
@@ -10,17 +10,17 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description: str = f.read()
 
 
-with open("real2sim/requirements.txt", "r", encoding="utf-8") as f:
+with open("retos/requirements.txt", "r", encoding="utf-8") as f:
     requirements: list[str] = f.read().splitlines()
 
 
-with open("real2sim/requirements-dev.txt", "r", encoding="utf-8") as f:
+with open("retos/requirements-dev.txt", "r", encoding="utf-8") as f:
     requirements_dev: list[str] = f.read().splitlines()
 
 
-with open("real2sim/__init__.py", "r", encoding="utf-8") as fh:
+with open("retos/__init__.py", "r", encoding="utf-8") as fh:
     version_re = re.search(r"^__version__ = \"([^\"]*)\"", fh.read(), re.MULTILINE)
-assert version_re is not None, "Could not find version in real2sim/__init__.py"
+assert version_re is not None, "Could not find version in retos/__init__.py"
 version: str = version_re.group(1)
 
 
